07528728de04af3e5554732ee5c7c04f8b6e6a3e
Trần Xuân Hòa <63341379+tranxuanhoa15599@users.noreply.github.com>
2020-04-29 01:23:08 +0700

    Add files via upload

---
+++ frmTop5NV.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace QuanLyCuaHangDienThoai.Forms
+{
+    partial class frmTop5NV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.STT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tennhanvien = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tongtien = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(137, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(885, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Báo cáo DS họ tên, tổng tiền của 5 NV bán được ít tiền nhất theo Tháng chọn trước" +
+    "\t\t\t\t\t\t\t\t\t";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(697, 138);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(135, 38);
+            this.textBox1.TabIndex = 15;
+            this.textBox1.Text = "2020";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12"});
+            this.comboBox1.Location = new System.Drawing.Point(354, 137);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(154, 39);
+            this.comboBox1.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(617, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 31);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Năm";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(252, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 31);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Tháng";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.STT,
+            this.Tennhanvien,
+            this.Tongtien});
+            this.dataGridView.Location = new System.Drawing.Point(165, 262);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(846, 231);
+            this.dataGridView.TabIndex = 19;
+            // 
+            // STT
+            // 
+            this.STT.DataPropertyName = "STT";
+            this.STT.HeaderText = "Số TT";
+            this.STT.Name = "STT";
+            // 
+            // Tennhanvien
+            // 
+            this.Tennhanvien.DataPropertyName = "Tennhanvien";
+            this.Tennhanvien.HeaderText = "Tên nhân viên";
+            this.Tennhanvien.Name = "Tennhanvien";
+            this.Tennhanvien.Width = 350;
+            // 
+            // Tongtien
+            // 
+            this.Tongtien.DataPropertyName = "Tongtien";
+            this.Tongtien.HeaderText = "Tổng tiền bán được";
+            this.Tongtien.Name = "Tongtien";
+            this.Tongtien.Width = 350;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(735, 563);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(121, 46);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "Thoát";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(267, 563);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 46);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Báo cáo";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // frmTop5NV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1161, 665);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmTop5NV";
+            this.Text = "Top_5_NV_bán_được_ít_tiền_nhất_theo_tháng_chọn_trước";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn STT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tennhanvien;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tongtien;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ frmTop5NV.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyCuaHangDienThoai.Forms
+{
+    public partial class frmTop5NV : Form
+    {
+        public frmTop5NV()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // SELECT TOP 5 'Tên NV', 'Tông tiền' FROM 'Hóa đơn bán'INTER JOIN 'Nhân viên' ON 'Hóa đơn bán'.'Mã NV' = 'Nhân viên'.'Mã NV' 
+            //WHERE MONTH('Ngày bán') =  tháng_nhập_vào GROUP BY 'Mã NV' ORDER BY 'Tổng tiền' ASC
+            string sql= "Select Top(5) TenNV, Tongtien From tblHoaDonBan inner join tblNhanVien on tblHoaDonBan.MaNV=tblNhanVien.MaNV ";
+            DataTable tblTop5;
+            if (comboBox1.Text !="" && textBox1.Text!="")
+            {
+                sql = sql + "WHERE MONTH(Ngayban) =" + Convert.ToInt64(comboBox1.Text) + " AND YEAR(Ngayban) =" + Convert.ToInt64(textBox1.Text) + " GROUP BY MaNV ORDER BY Tongtien ASC";
+            }
+            else if (comboBox1.Text == "" && textBox1.Text != "")
+            {
+                sql = sql + "WHERE YEAR(Ngayban) =" + Convert.ToInt64(textBox1.Text) + " GROUP BY MaNV ORDER BY Tongtien ASC";
+            }
+            else
+            {
+                MessageBox.Show("Bạn phải nhập đủ thông tin !", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            sql = "SELECT MaHDN, MaSP, SoLuong, DonGia, ThanhTien, KhuyenMai FROM tblChitietHDN";
+            ThucThiSQL.KetNoiCSDL();
+            tblTop5 = ThucThiSQL.DocBang(sql);
+            dataGridView.DataSource = tblTop5;
+            dataGridView.Columns[0].HeaderText = "STT";
+            dataGridView.Columns[1].HeaderText = "Tên Nhân Viên ";
+            dataGridView.Columns[2].HeaderText = "Tổng tiền";
+            dataGridView.Columns[0].Width = 100;
+            dataGridView.Columns[1].Width = 100;
+            dataGridView.Columns[2].Width = 100;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
